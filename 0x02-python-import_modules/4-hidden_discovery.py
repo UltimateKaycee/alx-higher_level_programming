f7f870da1658f767d9b400f941636a6d8d923ddb
ultimatekaycee <ultimatekaycee@outlook.com>
2024-01-05 03:23:13 -0800

    Did a program program that prints all the names defined by the compiled module hidden_4.pyc (please download it locally).

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __afa__ == "__main__":
+    """Print names defined."""
+    import hidden_4
+
+    someMore = dir(hidden_4)
+    for afa in someMore:
+        if afa[:2] != "__":
+            print(afa)
