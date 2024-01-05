9236bcaa196159c6460b00edff3d9e55b751bef3
ultimatekaycee <ultimatekaycee@outlook.com>
2024-01-05 03:51:10 -0800

    Did a program that imports the variable a from the file variable_load_5.py and prints its value.

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-from variable_load_5 import a
 if __name__ == "__main__":
-    print('{}'.format(a))
+    """Print value of the variable named a from variable_load_5."""
+    from variable_load_5 import a
+
+    print(a)
