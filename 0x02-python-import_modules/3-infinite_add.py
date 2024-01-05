711ef8f943cec9793f69926006a6f8d0d20d1659
ultimatekaycee <ultimatekaycee@outlook.com>
2024-01-05 03:29:07 -0800

    Did a program that prints the result of the addition of all arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """Print out sum of arguments."""
+    """Print sum of arguments."""
     import sys
 
-    sum = 0
+    all = 0
     for start in range(len(sys.argv) - 1):
-        sum += int(sys.argv[start + 1])
-    print("{}".format(sum))
+        all += int(sys.argv[start + 1])
+    print("{}".format(all))
