3716171d3ae912986577d20ac8703e2dc1ef8ca5
ultimatekaycee <ultimatekaycee@outlook.com>
2024-01-05 03:43:09 -0800

    Did a program that imports the variable a from the file variable_load_5.py and prints its value.

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """Print all names defined by hidden_4 module."""
+    import hidden_4
+
+    someNames = dir(hidden_4)
+    for name in someNames:
+        if name[:2] != "__":
+            print(name)
