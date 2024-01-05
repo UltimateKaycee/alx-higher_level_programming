f1be3eb5cb38b2e72cc32fbbcd55b12a7a1817b7
ultimatekaycee <ultimatekaycee@outlook.com>
2024-01-05 04:12:50 -0800

    Did a Python function def magic_calculation(a, b): that does exactly the same as the following Python

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -2,12 +2,10 @@
 def magic_calculation(a, b):
     """Match the provided bytecode."""
     from magic_calculation_102 import add, sub
-
     if a < b:
         c = add(a, b)
         for start in range(4, 6):
             c = add(c, start)
         return (c)
-
     else:
         return(sub(a, b))
