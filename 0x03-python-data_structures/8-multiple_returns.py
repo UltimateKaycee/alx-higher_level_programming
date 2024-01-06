2c99240c67e226fa21a639dbc340434df4b4718a
ultimatekaycee <ultimatekaycee@outlook.com>
2024-01-06 06:59:29 -0800

    redid task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    the_length = len(sentence)
-    char_one = sentence[0] if the_length > 0 else "None"
-    the_tup = the_length, char_one
-    return(the_tup)
+    if sentence == "":
+        return (0, None)
+    return (len(sentence), sentence[0])
