0d4bee042222c896541a4bb8231774a6c7006c51
ultimatekaycee <ultimatekaycee@outlook.com>
2024-01-23 17:59:45 -0800

    Did a class Square that defines a square by: (based on 0-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""We define a class named Square."""
+
+
+class Square:
+    """The square class."""
+
+    def __init__(self, size):
+        """The Square class is initialized.
+
+           size (int): size of the square.
+        """
+        self.__size = size
