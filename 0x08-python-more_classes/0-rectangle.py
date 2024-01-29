3af4c8eb4693b506d75353369b6c166fb5576805
ultimatekaycee <ultimatekaycee@outlook.com>
2024-01-29 09:02:05 -0800

    Did an empty class Rectangle that defines a rectangle:

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""A Class, empty that defines a Rectangle."""
+
+
+class Rectangle:
+    """A rectangle class definition."""
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+This is a directory about More Python Classes in the root of the repository
