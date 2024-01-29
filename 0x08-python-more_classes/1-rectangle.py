87dbc623469e2bfc617775cb49a424b3b573c23a
ultimatekaycee <ultimatekaycee@outlook.com>
2024-01-29 09:10:50 -0800

    Did a class Rectangle that defines a rectangle by: (based on 0-rectangle.py)

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""A Class that defines a rectangle based on another."""
+
+
+class Rectangle:
+    """A rectangle class definition."""
+
+    def __init__(self, width=0, height=0):
+        """We initialize the new Rectangle class.
+
+        Argsuments are width (int) of our new rectangle.
+            and height (int) of our new rectangle.
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Getting and setting width of rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must gt/equal 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getting and seting height of rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must gt/equal 0")
+        self.__height = value
