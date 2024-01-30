f81e55fa0ab9806df3ad98386e068f6b31b4ad53
ultimatekaycee <ultimatekaycee@outlook.com>
2024-01-30 10:58:30 -0800

    Did task 30

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -7,6 +7,9 @@ class LockedClass:
     __slots__ = ('first_name',)
 
     def __init__(self, first_name):
+"""
+    The function
+"""
         self.first_name = first_name
 
 locked_obj = LockedClass('Ultimate')
