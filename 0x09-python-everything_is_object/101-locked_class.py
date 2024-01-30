f50b3209097083c77d0eb95dce5fdf67f047de12
ultimatekaycee <ultimatekaycee@outlook.com>
2024-01-30 12:02:00 -0800

    Reviewed task 30 again

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
-REPLACE
-    return (0)
-print(REPLACE)
+"""
+    Defining LockedClass with no class or object attribute.
+"""
+
+
+class LockedClass:
+    __slots__ = ('first_name',)
+
+    def __init__(self, first_name):
+        self.first_name = first_name
+
+
+the_locked = LockedClass('Ultimate')
+print(the_locked.first_name)
