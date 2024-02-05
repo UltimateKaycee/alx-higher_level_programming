e2fb00d0c4392589d45d12507cf1c4899d0d9c49
ultimatekaycee <ultimatekaycee@outlook.com>
2024-02-05 04:44:10 -0800

    Did task 6 and 5

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Class definition for empty BaseGeometry."""
+
+
+class BaseGeometry:
+    """Class for base geometry."""
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Class definition for base geometry."""
+
+
+class BaseGeometry:
+    """A class for base geometry."""
+
+    def area(self):
+        """method to raise exception."""
+        raise Exception("area() is not implemented")
