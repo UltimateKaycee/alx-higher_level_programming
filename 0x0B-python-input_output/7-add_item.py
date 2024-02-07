54d8eba5651145defedc84c9d6d27661e6c2fd05
ultimatekaycee <ultimatekaycee@outlook.com>
2024-02-06 17:47:39 -0800

    pycode checked 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,14 +2,17 @@
 """Script to aAdd all arguments to Python list and save them to a file."""
 import sys
 
-if __name__ == "__main__":
+
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = __import__(
             '6-load_from_json_file').load_from_json_file
 
+if __name__ == "__main__":
     try:
         elements = load_from_json_file("add_item.json")
-    except FileNotFoundError:
+    except:
+        pass
         elements = []
-    elements.extend(sys.argv[1:])
+    for c in argv[1:]:
+        elements.append(c)
     save_to_json_file(elements, "add_item.json")
